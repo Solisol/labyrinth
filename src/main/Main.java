ee2dcc8778cd364e09bd2f152b94add3c3e6329b
Sol <jesandeit@gmail.com>
2015-01-15 18:20:38 +0100

    prints maze to a jpanel

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,20 @@
 package main;
 
 import main.creator.RecursiveBacktrackerMazeCreator;
+import main.world.Board;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        RecursiveBacktrackerMazeCreator maze = new RecursiveBacktrackerMazeCreator(10, 10);
-        maze.generateMaze();
-        maze.printMaze();
+        JFrame frame = new JFrame();
+        frame.setLayout(new GridLayout(1,1));
+        frame.add(new Board(100, 100));
+        frame.setSize(801,801);
+        frame.setTitle("Hello frameu laburinthu");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
--- src/main/world/Board.java
+++ src/main/world/Board.java
@@ -2,16 +2,57 @@ package main.world;
 
 import main.creator.MazeCreator;
 import main.creator.RecursiveBacktrackerMazeCreator;
+import main.models.Vertex;
 
+import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
-public class Board extends Frame {
+public class Board extends JPanel {
 
+    private final int width;
+    private final int height;
+    private final int length;
     private MazeCreator maze;
 
-    public void draw() {
-        maze = new RecursiveBacktrackerMazeCreator(10, 10);
+    public Board(int width, int height) {
+        this.width = width;
+        this.height = height;
+        length = width * height;
+        maze = new RecursiveBacktrackerMazeCreator(width, height);
         maze.generateMaze();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        g.setColor(Color.green);
+
+        int tileWidth = getWidth() / width;
+        int tileHeight = getHeight() / height;
+
+        //Top line of maze
+        g.drawLine(0, 0, tileWidth * width, 0);
+
+
+        ArrayList<Vertex> nodes = maze.getPaths().getNodes();
+        for (int i = 0; i < length; i++) {
+            int x = i % width;
+            int y = i / width;
+            Vertex node = nodes.get(i);
+            if (maze.isLeftEdge(node)) {
+                g.drawLine(tileWidth * x, tileHeight * y, tileWidth * x, tileHeight * (y + 1));
+            }
+            if (!maze.hasDown(node)) {
+                g.drawLine(tileWidth * x, tileHeight * (y + 1), tileWidth * (x + 1), tileHeight * (y + 1));
+            }
+            if (!maze.hasRight(node)) {
+                g.drawLine(tileWidth * (x + 1), tileHeight * y, tileWidth * (x + 1), tileHeight * (y + 1));
+            }
+        }
+        maze.printMaze();
     }
 
 }
