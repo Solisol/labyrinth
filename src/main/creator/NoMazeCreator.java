01f71b4560b6bb19f2f6c68cf6c1997f57f72dd9
Jessica Andersson <ja@speedledger.se>
2015-01-16 15:30:35 +0100

    Generates a full path, ie a maze without inner walls

--- src/main/creator/NoMazeCreator.java
+++ src/main/creator/NoMazeCreator.java
@@ -1,9 +1,6 @@
 package main.creator;
 
 import main.models.Graph;
-import main.models.Vertex;
-
-import java.util.ArrayList;
 
 public class NoMazeCreator extends MazeCreator {
 
@@ -15,35 +12,8 @@ public class NoMazeCreator extends MazeCreator {
 
     @Override
     public void generateMaze() {
-        paths = getPathToAllNeighbours();
+        paths = Graph.getFullGridGraph(width, height);
     }
 
-    private Graph getPathToAllNeighbours() {
-        Graph graph = new Graph(length);
-        for (int i = 0; i < length; i++) {
-            Vertex node = graph.getNodeInPosition(i);
-            node.setOutNodes(getAllNeighbours(node, graph));
-            graph.updateNode(node);
-        }
-        return graph;
-    }
 
-    private ArrayList<Vertex> getAllNeighbours(Vertex node, Graph graph) {
-        ArrayList<Vertex> neighbours = new ArrayList<Vertex>();
-
-        int index = node.getIndex();
-        if (index % width != 0) {
-            neighbours.add(graph.getNodeInPosition(index - 1));
-        }
-        if (index >= width) {
-            neighbours.add(graph.getNodeInPosition(index - width));
-        }
-        if (index  < length - width) {
-            neighbours.add(graph.getNodeInPosition(index + width));
-        }
-        if (index % width != (width - 1)) {
-            neighbours.add(graph.getNodeInPosition(index + 1));
-        }
-        return neighbours;
-    }
 }
